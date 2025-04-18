690c0ef04a4cbb13dc99e0d76b54116ca204179d
GuilhermeFujita <fujitaguilherme@gmail.com>
2025-04-18 12:05:23 -0300

    Add cors configuration

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/GuilhermeFujita/nlw_notes_api/cmd/modules"
+	"github.com/go-chi/cors"
 	"go.uber.org/fx"
 )
 
@@ -19,11 +20,20 @@ func main() {
 }
 
 func StartServer(lc fx.Lifecycle, router http.Handler) {
+	corsMiddleware := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"},
+		AllowedMethods: []string{"GET", "POST", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"Content-Type"},
+	})
+
+	// 2) envolva o seu router com o middleware de CORS
+	handler := corsMiddleware.Handler(router)
+
 	lc.Append(fx.Hook{
 		OnStart: func(ctx context.Context) error {
 			go func() {
 				log.Println("Server is starting at :9090")
-				http.ListenAndServe(":9090", router)
+				http.ListenAndServe(":9090", handler)
 			}()
 			return nil
 		},
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.24.1
 
 require (
 	github.com/go-chi/chi/v5 v5.2.1
+	github.com/go-chi/cors v1.2.1
 	github.com/go-chi/render v1.0.3
 	github.com/joho/godotenv v1.5.1
 	go.uber.org/fx v1.23.0
